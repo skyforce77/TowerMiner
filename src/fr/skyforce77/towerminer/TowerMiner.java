96bede28fa33649ecb27244c1d7d2b55ea9096dd
skyforce77 <arnaud.bruyer@gmail.com>
2014-08-04 20:52:15 +0200

    Beta 0.5d

--- src/fr/skyforce77/towerminer/TowerMiner.java
+++ src/fr/skyforce77/towerminer/TowerMiner.java
@@ -35,7 +35,7 @@ public class TowerMiner {
 	public static int neededlauncherversion = 13;
 	public static int actuallauncherversion = -1;
 
-	public static boolean dev = true;
+	public static boolean dev = false;
 	public static String version = "Beta 0.5d";
 
 	public static boolean launcherupdateneeded = true;
